c505d37270f3e07bb70d604260c5bf8efb9606d3
Michal Harakal <michal.harakal@googlemail.com>
2024-08-30 20:20:42 +0200

    Add simple gguf file reader test in python

--- test/pytorch_test_data_io/tensor_io.py
+++ test/pytorch_test_data_io/tensor_io.py
@@ -1,19 +1,33 @@
+from typing import List, Tuple
+
 from gguf import GGUFWriter, GGUFReader  # noqa: E402
 import numpy as np  # noqa: E402
+import torch  # noqa: E402
 
 
 # Example usage:
-def tensor_writer(gguf_file_path: str, tensor_name, tensor):
+def tensor_writer(gguf_file_path: str, tensor_name, tensor: torch.Tensor):
     # Example usage with a file
     gguf_writer = GGUFWriter(gguf_file_path, "llama")
 
-    gguf_writer.add_tensor(tensor_name, tensor)
+    gguf_writer.add_tensor(tensor_name, tensor.cpu().numpy(), np.array(tensor.shape))
     gguf_writer.write_header_to_file()
     gguf_writer.write_kv_data_to_file()
     gguf_writer.write_tensors_to_file()
 
+    gguf_writer.close()
 
-def tensor_reader(gguf_file_path: str):
+
+def to_tensor(flatten_data, shape) -> torch.Tensor:
+    tensor_shape = tuple(shape[::-1].copy())  # Convert shape memmap to a tuple, which should be (3, 4)
+
+    # Reshape the flat data according to the loaded shape
+    reshaped_data = np.reshape(flatten_data, tensor_shape)
+
+    return torch.from_numpy(reshaped_data.copy())
+
+
+def tensor_reader(gguf_file_path: str) -> List[Tuple[str, torch.Tensor]]:
     # Example usage with a file
     reader = GGUFReader(gguf_file_path)
 
@@ -21,11 +35,10 @@ def tensor_reader(gguf_file_path: str):
     tensor_info_format = "{:<30} | Shape: {:<15} | Size: {:<12} | Quantization: {}"
     print(tensor_info_format.format("Tensor Name", "Shape", "Size", "Quantization"))
     print("-" * 80)
-    return [(tensor.name, tensor.data) for tensor in reader.tensors]
+    return [(tensor.name, to_tensor(tensor.data, tensor.shape)) for tensor in reader.tensors]
 
 
 if __name__ == '__main__':
     tensor_writer("example.gguf", "x", np.array([-4.0], dtype=np.float32))
-    a = tensor_reader("example.gguf")
+    (a, b) = tensor_reader("example.gguf")
     print(a)
-
--- test/test_gguf_tensor_io.py
+++ test/test_gguf_tensor_io.py
@@ -1,15 +1,68 @@
+import numpy as np
 import torch
 from pytorch_test_data_io.tensor_io import tensor_writer, tensor_reader
 
 import unittest
 
 
-class TestStringMethods(unittest.TestCase):
+class TestTensorIoWithGguf(unittest.TestCase):
+
+    def test_simple_tensor_io(self):
+        x = torch.Tensor(np.array([-4.0], dtype=np.float32))
+        tensor_writer("example.gguf", "x", x)
+
+        tensors = tensor_reader("example.gguf")
+        name = tensors[0][0]
+        file_value_x = tensors[0][1]
+
+        assert len(tensors) == 1
+        assert name == 'x'
+        assert x.data == file_value_x.data
+        assert x.shape == file_value_x.shape
+
+    def test_vector_tensor_io(self):
+        x = torch.Tensor(np.array([-4.0, 5.0], dtype=np.float32))
+        tensor_writer("example1.gguf", "x", x)
+
+        tensors = tensor_reader("example1.gguf")
+        name = tensors[0][0]
+        file_value_x = tensors[0][1]
+
+        assert len(tensors) == 1
+        assert name == 'x'
+        assert torch.equal(x, file_value_x)
+        assert x.shape == file_value_x.shape
+
+    def test_matrix_tensor_io(self):
+        x = torch.Tensor(np.array([[1, 2, 3], [4, 5, 6]], dtype=np.float32))
+        tensor_writer("example2.gguf", "x", x)
+
+        tensors = tensor_reader("example2.gguf")
+        name = tensors[0][0]
+        file_value_x = tensors[0][1]
+
+        assert len(tensors) == 1
+        assert name == 'x'
+        assert torch.equal(x, file_value_x)
+        assert x.shape == file_value_x.shape
 
     def test_tensor_io(self):
-        x = torch.Tensor([-4.0]).float32()
-        tensor_writer("example.gguf", "x", x)
-        (name, file_value_x) = tensor_reader("example.gguf")
+        x = torch.Tensor(np.array([
+            [
+                [1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]
+            ],
+            [
+                [11, 12, 13], [14, 15, 16], [17, 18, 19], [17, 18, 19]
+            ]
+        ], dtype=np.float32))
 
+        tensor_writer("example3.gguf", "x", x)
+
+        tensors = tensor_reader("example3.gguf")
+        name = tensors[0][0]
+        file_value_x = tensors[0][1]
+
+        assert len(tensors) == 1
         assert name == 'x'
-        assert x.data == file_value_x.data.item()
+        assert torch.equal(x, file_value_x)
+        assert x.shape == file_value_x.shape
